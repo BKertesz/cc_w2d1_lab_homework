c98d80abe39c8b605b7a8a4edbcdb8bbd850ce13
BKertesz <kertesz.balazs92@gmail.com>
2018-03-12 15:06:37 +0000

    Finished version

--- homework_w2_d1.rb
+++ homework_w2_d1.rb
@@ -40,12 +40,73 @@ class Team
     @team_name = team_name
     @players = players
     @coach = coach
+    @points = 0
   end
 
   def add_new_player(new_player)
     return players.push(new_player)
   end
+
+  def check_if_player(player_name)
+    for name in @players
+      return true if name == player_name
+    end
+  end
+
+  def update_score(match_info)
+    return @points += 1 if match_info == "won"
+  end
+
+
 end
 # End of Part 2
+# Start of Extension
+class Library
+  attr_reader :books
+  def initialize()
+    @books = [
+      {
+        title: "lord_of_the_rings",
+        rental_details: {
+         student_name: "Jeff",
+         date: "01/12/16"
+        }
+      }
+    ]
+  end
+
+  def check_book_info_by_title(title)
+    for book in @books
+      return book if book[:title] == title
+    end
+  end
+
+  def check_rental_information(title)
+    for book in @books
+      return book[:rental_details] if book[:title] == title
+    end
+  end
+
+  def add_new_book(title)
+    array = {
+      title: title,
+      rental_details: {
+        student_name: "",
+        date: ""
+      }
+    }
+    @books.push(array)
+  end
+
+  def update_book_info(title,student,date)
+    for book in @books
+      if book[:title] == title
+        book[:rental_details][:student_name] = student
+        book[:rental_details][:date] = data
+      end
+    end
+  end
+end
+# End of Extension
 
 # EOF
